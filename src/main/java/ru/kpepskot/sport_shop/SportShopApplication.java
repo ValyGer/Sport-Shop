0c2b81b14ae7c2abeb3754b9b716616050591f8d
Valentin <valentin.geras@yandex.ru>
2024-09-10 01:47:50 +0300

    Добавлена структура файлов и добавлена БД

---
+++ src/main/java/ru/kpepskot/sport_shop/SportShopApplication.java
@@ -0,0 +1,13 @@
+package ru.kpepskot.sport_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kpepskot/sport_shop/SportShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kpepskot.sport_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
